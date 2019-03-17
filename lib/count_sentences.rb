4ff2b1cc38058cf2c1209c22d370c0d4df3de308
Danielle Alba <danib.sgh@gmail.com>
2019-03-17 19:29:41 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[self.length-1] == "." ? true : false
   end
 
   def question?
-
+    self[self.length-1] == "?" ? true : false
   end
 
   def exclamation?
+    self[self.length-1] == "!" ? true : false
 
   end
 
   def count_sentences
-
+    puts self
+    self.split(/[.!]/).length
   end
 end
+
+# binding.pry
